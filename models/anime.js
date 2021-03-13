fd7981126fbb4488e70ea910d8aa74c4ceabafd3
ADAM ANDREASON <adamandreason@gmail.com>
2021-03-13 01:51:18 -0700

    fixed error with missing ep, score

--- models/anime.js
+++ models/anime.js
@@ -5,9 +5,9 @@ const animeSchema = new Schema({
     title: {type: String, required: true},
     image_url: {type: String, default: "https://via.placeholder.com/225x319.jpeg/0000FF/808080?text=Oops!+Image+not+Found"},
     type: {type: String, required: true},
-    episodes: {type: Number, required: true},
+    episodes: {type: Number, default: "0"},
     synopsis: {type: String, default: "No synopsis provided."},
-    score:  {type: Number, required: true},
+    score:  {type: Number, default: "0"},
     mal_id: {type: Number, required: true}
 })
 
